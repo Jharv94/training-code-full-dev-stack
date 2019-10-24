503525bb8f2cb67899b1e583e8e68f9d446e32ba
john harvey <johnharvey94@hotmail.com>
2019-10-24 15:37:23 -0500

    added firstCSharp Program

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using sc = System.Console; //import with alias
+
+namespace HelloWorld.Client
+{
+  /// <summary>
+  /// Contains start of the program
+  /// </summary>
+  internal class Program
+  {
+    /// <summary>
+    /// Starts the program
+    /// </summary>
+      private static void Main()
+      {
+          sc.WriteLine("Hello World!");
+      }
+  }
+}
